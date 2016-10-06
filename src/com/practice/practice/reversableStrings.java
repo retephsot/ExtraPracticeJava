f4a0c07b2b4fd7d2e34f916b54b4be71f1040ed5
retephsot <synhlee@yahoo.com>
2016-10-05 21:48:20 -0400

    Adding reversableStrings class

---
+++ src/com/practice/practice/reversableStrings.java
@@ -0,0 +1,39 @@
+package com.practice.practice;
+
+
+public class reversableStrings {
+
+	public static int length;
+	//The test string
+	public static String str = "mom mom mom";
+	public static String results;
+	public static boolean endResults;
+	
+	public static void main(String[] args) {
+		
+		endResults = inspectString(str);
+		System.out.println("The result is " + endResults);
+	}
+		
+	public static boolean inspectString(String str)
+	{
+		results = "";
+		length = str.length();
+		
+		for(int i = length; i>0; i--)
+		{
+			results += str.charAt(i-1);
+		}
+		
+		System.out.println("The string value reversed is " + results);
+		
+		if (results.equals(str))
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
+}
