4451c63564bffcab4f5c129f0e741acd4a474873
retephsot <synhlee@yahoo.com>
2018-06-23 05:15:26 -0400

    messing around

---
+++ src/com/practice/practice/codeChallenge.java
@@ -0,0 +1,43 @@
+package com.practice.practice;
+
+public class codeChallenge {
+
+	public static int length;
+	public static int resultNumber;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int[] intArray = new int[] {1,1,2,3,3};
+
+		System.out.println("the array values are " + intArray[4]);
+		
+		resultNumber = lonelyInteger(intArray);
+		
+		System.out.println("The value of resultNumber is " + resultNumber);
+		
+	}
+
+	public static int lonelyInteger(int[] intArray) {
+	int length = intArray.length;
+	System.out.println("The length value is " + length);
+    int lonelyNumber = 0;
+    
+    for (int i = 0; i < length-1; i++)
+    {
+    int compare = intArray[i];
+    
+    	for (int j = 0; j < length-1; j++)
+    	{
+    		if(compare != intArray[j])
+    			{
+    				lonelyNumber = compare;
+    			}
+    
+    	}
+    }
+
+    System.out.println("The lonelyNumber is " + lonelyNumber);   
+    return lonelyNumber;
+	}
+
+}
