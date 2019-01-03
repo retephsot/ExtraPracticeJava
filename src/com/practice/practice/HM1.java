4e8fa5e63c172398f2a2cf56fa6e22a235ea7818
retephsot <synhlee@yahoo.com>
2019-01-03 08:55:20 -0500

    more werk

--- src/com/practice/practice/HM1.java
+++ src/com/practice/practice/HM1.java
@@ -7,27 +7,67 @@ public class HM1 {
 
 	public static void main(String[] args) {
 		
-		oleE.put(6, "whats yo name fool");
-		oleE.put(7, "funky fresh easy e");
-		
-		for(Map.Entry<Integer, String> entry : oleE.entrySet()) {
-			
-			int i = entry.getKey();
-			String value = entry.getValue();
-			System.out.println("key and value for oleE consists of " + i + " : " + value);
-			
-		}
+		MapHelper mh = new MapHelper();
+		NWAHMap nwaMap = new NWAHMap();
 
-	}
-	
-	
-	public static HashMap<Integer, String> oleE = new HashMap<Integer, String>();
-	static {
-		
+		HashMap<Integer, String> oleE = new HashMap<Integer, String>();
+
 		oleE.put(1, "crusing down the street in my 6 4");
 		oleE.put(2, "jocking da bi, slappin they h");
 		oleE.put(3, "went to da park to get to school");
 		oleE.put(4, "knuckle head out there co shooting some hoops");
-	}
+		oleE.put(6, "whats yo name fool");
+		oleE.put(7, "funky fresh easy e");
+		
+		
+		nwaMap.getDreMap().put(1, "yo dre lets kick it on the one");
+		nwaMap.getDreMap().put(2, "and we don't stop");
+		nwaMap.getDreMap().put(3, "makin record, the people co checkin and respectin it");
+		nwaMap.getDreMap().put(4, "cus it rocks the sells won't stop");
 
+		System.out.println("the key for value whats yo name fool is " + getKey("whats yo name fool", oleE));
+
+		for (Map.Entry<Integer, String> entry : oleE.entrySet()) {
+
+			int i = entry.getKey();
+			String value = entry.getValue();
+			System.out.println("key and value for oleE consists of " + i + " : " + value);
+
+		}
+
+		System.out.println("Alternative way to get key value set is ---------------------");
+		mh.printKeyVal(oleE);
+
+		for (Map.Entry<Integer, String> ent : oleE.entrySet()) {
+			if (ent.getValue().equals("whats yo name fool")) {
+				ent.setValue("What is your name sir?");
+			}
+		}
+
+		System.out.println("now the key value set consists of the following ---------- ");
+
+		mh.printKeyVal(oleE);
+		
+		System.out.println(" printing out key val pairs for nwaHMap ---------------");
+		
+		mh.printKeyVal(nwaMap.getDreMap());
+
+	}
+	
+	
+
+	public static int getKey(String val, Map<Integer, String> myMap) {
+
+		int keyVal = 0;
+
+		for (Map.Entry ent : myMap.entrySet()) {
+			if (ent.getValue().equals(val)) {
+				System.out.println("The key val you want is " + ent.getKey());
+				keyVal = (int) ent.getKey();
+			}
+		}
+
+		return keyVal;
+	}
+					
 }
---
+++ src/com/practice/practice/NWAHMap.java
@@ -0,0 +1,18 @@
+package com.practice.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NWAHMap {
+	
+	private Map<Integer, String> DreMap = new HashMap<Integer, String>();
+
+	public Map<Integer, String> getDreMap() {
+		return DreMap;
+	}
+
+	public void setDreMap(Map<Integer, String> dreMap) {
+		DreMap = dreMap;
+	}
+
+}
